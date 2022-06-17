79bd9cb7abdef9aacecb34c5ae8dc0c2f2f014d4
Santiago Gómez Grajales <magoratoga@gmail.com>
2022-06-17 11:42:45 -0500

    Added counter methods using redux slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	decrement,
+	increment,
+	incrementBy,
+} from './store/slices/counter/counterSlice';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const { counter } = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
 
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hello Vite + React!</p>
+				<p>count is: {counter}</p>
 				<p>
+					<button type='button' onClick={() => dispatch(increment())}>
+						Increment
+					</button>
+					<button type='button' onClick={() => dispatch(decrement())}>
+						Decrement
+					</button>
 					<button
 						type='button'
-						onClick={() => setCount((count) => count + 1)}>
-						count is: {count}
+						onClick={() => dispatch(incrementBy(2))}>
+						Increment by 2
 					</button>
 				</p>
 			</header>
--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -11,10 +11,16 @@ export const counterSlice = createSlice({
 			// doesn't actually mutate the state because it uses the Immer library,
 			// which detects changes to a "draft state" and produces a brand new
 			// immutable state based off those changes
-			state.value += 1;
+			state.counter += 1;
+		},
+		incrementBy: (state, action) => {
+			state.counter += action.payload;
+		},
+		decrement: (state) => {
+			state.counter -= 1;
 		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions;
+export const { increment, incrementBy, decrement } = counterSlice.actions;
